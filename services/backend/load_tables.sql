bfbd04fa086998fd2726c682296ee3ce07cd72c1
Kaushik <kaushik@kaushik>
2025-03-29 18:36:37 +0530

    @CSE

--- services/backend/load_tables.sql
+++ services/backend/load_tables.sql
@@ -1,7 +1,7 @@
-load data local infile 'Product_Table.csv' into table Product_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
 load data local infile 'Branch_Table.csv' into table Branch_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
+load data local infile 'Product_Table.csv' into table Product_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
 load data local infile 'Supplier_Table.csv' into table Supplier_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
-load data local infile 'Entry_Table.csv' into table Entry_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
-load data local infile 'Entry_Items.csv' into table Entry_Items fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
 load data local infile 'Order_Table.csv' into table Order_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
 load data local infile 'Order_Items.csv' into table Order_Items fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
+load data local infile 'Entry_Table.csv' into table Entry_Table fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
+load data local infile 'Entry_Items.csv' into table Entry_Items fields terminated by ',' enclosed by '"' lines terminated by '\n' ignore 1 rows;
